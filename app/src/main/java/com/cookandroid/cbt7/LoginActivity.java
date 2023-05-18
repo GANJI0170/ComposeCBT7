6aedcce7759e38b803d1a5fd77b26bc7e7185552
tw741 <osj5866@gmail.com>
2023-05-18 23:57:05 +0900

    데이터베이스 연동 + 리사이클러뷰

--- app/src/main/java/com/cookandroid/cbt7/LoginActivity.java
+++ app/src/main/java/com/cookandroid/cbt7/LoginActivity.java
@@ -1,15 +1,37 @@
 package com.cookandroid.cbt7;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TabHost;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<articleList> arrayList;
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,7 +81,6 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-
         //상단 탭호스트 2개 버전
 //        EditText editkw1 = (EditText) findViewById(R.id.editkw1);
 //        editkw1.setOnClickListener(new View.OnClickListener(){
@@ -80,5 +101,39 @@ public class LoginActivity extends AppCompatActivity {
 //                startActivity(in);
 //            }
 //        });
+
+        recyclerView = findViewById(R.id.recyclerView1);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(getApplicationContext(),new LinearLayoutManager(this).getOrientation());
+        recyclerView.addItemDecoration(dividerItemDecoration);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        arrayList = new ArrayList<>(); //articleList 객체를 어댑터 쪽으로 담을 arrayList
+
+        database = FirebaseDatabase.getInstance(); //파이어베이스 연동
+
+        databaseReference = database.getReference("found_article"); // DB 테이블 연결
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //파이어베이스 데이터를 받아오는 곳
+                arrayList.clear(); //기존 배열 초기화
+                for (DataSnapshot snapshot1 : snapshot.getChildren()) { //반복문으로 데이터 리스트 추출
+                    articleList articleList = snapshot1.getValue(com.cookandroid.cbt7.articleList.class); //arrayList 객체에 데이터 담기
+                    arrayList.add(articleList);
+                }
+                adapter.notifyDataSetChanged(); //리스트 저장 및 새로고침
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e("LoginActivity", String.valueOf(error.toException()));
+            }
+        });
+
+        adapter = new CustomAdaptor(arrayList, this);
+        recyclerView.setAdapter(adapter);
+
     }
 }
+
--- app/src/main/java/com/cookandroid/cbt7/articleList.java
+++ app/src/main/java/com/cookandroid/cbt7/articleList.java
@@ -1,54 +1,54 @@
 package com.cookandroid.cbt7;
 
 public class articleList {
-    private String articleImage;
-    private String articleTitle;
-    private String articlekeyword;
-    private String articlewriter;
-    private String articletime;
-    private int articleviews;
+    private String found_image;
+    private String found_title;
+    private String found_keyword;
+    private String found_id;
+    private String found_post_date;
+    private String found_hits;
 
     public articleList(){}
 
-    public String getArticleImage() {
-        return articleImage;
+    public String getFound_image() {
+        return found_image;
     }
-    public void setArticleImage(String articleImage) {
-        this.articleImage = articleImage;
-    }
-
-    public String getArticleTitle() {
-        return articleTitle;
-    }
-    public void setArticleTitle() {
-        this.articleTitle = articleTitle;
+    public void setFound_image(String articleImage) {
+        this.found_image = articleImage;
     }
 
-    public String getArticlekeyword() {
-        return articlekeyword;
+    public String getfound_title() {
+        return found_title;
     }
-    public void setArticlekeyword() {
-        this.articlekeyword = articlekeyword;
-    }
-
-    public String getArticlewriter() {
-        return articlewriter;
-    }
-    public void setArticlewriter() {
-        this.articlewriter = articlewriter;
+    public void setfound_title() {
+        this.found_title = found_title;
     }
 
-    public String getArticletime() {
-        return articletime;
+    public String getfound_keyword() {
+        return found_keyword;
     }
-    public void setArticletime() {
-        this.articletime = articletime;
+    public void setfound_keyword() {
+        this.found_keyword = found_keyword;
     }
 
-    public int getArticleviews() {
-        return articleviews;
+    public String getfound_id() {
+        return found_id;
     }
-    public void setArticleviews() {
-        this.articleviews = articleviews;
+    public void setfound_id() {
+        this.found_id = found_id;
+    }
+
+    public String getfound_post_date() {
+        return found_post_date;
+    }
+    public void setfound_post_date() {
+        this.found_post_date = found_post_date;
+    }
+
+    public String getfound_hits() {
+        return found_hits;
+    }
+    public void setfound_hits() {
+        this.found_hits = found_hits;
     }
 }
