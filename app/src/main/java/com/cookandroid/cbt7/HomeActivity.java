dec3079694f3e2981f28801c0d3b2590703eb5db
tw741 <osj5866@gmail.com>
2023-05-23 22:06:32 +0900

    하단바 + 데이터베이스

--- app/src/main/java/com/cookandroid/cbt7/HomeActivity.java
+++ app/src/main/java/com/cookandroid/cbt7/HomeActivity.java
@@ -10,7 +10,6 @@ import com.cookandroid.cbt7.database.articlefoundList;
 import com.cookandroid.cbt7.database.articlelostList;
 import com.cookandroid.cbt7.database.foundAdaptor;
 import com.cookandroid.cbt7.database.lostAdaptor;
-import com.cookandroid.cbt7.navigation.fragment_board;
 import com.cookandroid.cbt7.navigation.fragment_home;
 import com.cookandroid.cbt7.navigation.fragment_myarticle;
 import com.cookandroid.cbt7.navigation.fragment_write;
@@ -33,17 +32,16 @@ import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private RecyclerView recyclerView1, recyclerView2;
-    private RecyclerView.Adapter adapter1, adapter2;
-    private RecyclerView.LayoutManager layoutManager1, layoutManager2;
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
     private ArrayList<articlefoundList> arrayListfound;
     private ArrayList<articlelostList> arrayListlost;
     private FirebaseDatabase database;
-    private DatabaseReference databaseReference1, databaseReference2;
+    private DatabaseReference databaseReference;
     private fragment_home fragmentHome = new fragment_home();
     private fragment_myarticle fragmentMyarticle = new fragment_myarticle();
     private fragment_write fragmentWrite = new fragment_write();
-    private fragment_board fragmentboard = new fragment_board();
     private Button BTNlost, BTNfound;
     private LinearLayout l;
 
@@ -54,7 +52,7 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
 //      bottomNavigationView 설정
-        FrameLayout frameLayout = (FrameLayout) findViewById(R.id.framelayout1);
+//        FrameLayout frameLayout = (FrameLayout) findViewById(R.id.framelayout1);
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.menu_bottom_navigation);
         bottomNavigationView.setSelectedItemId(R.id.menuhome);
         bottomNavigationView.setOnNavigationItemSelectedListener(listener);
@@ -63,150 +61,35 @@ public class HomeActivity extends AppCompatActivity {
         l = (LinearLayout) findViewById(R.id.board);
         l.setVisibility(View.GONE);
 
-//      탭호스트 설정
-//        TabHost tabHost1 = (TabHost) findViewById(R.id.tabHost1);
-//        tabHost1.setup() ;
-//        TabHost.TabSpec ts1 = tabHost1.newTabSpec("Tab Spec 1");
-//        TabHost.TabSpec ts2 = tabHost1.newTabSpec("Tab Spec 2");
-//        TabHost.TabSpec ts3 = tabHost1.newTabSpec("Tab Spec 3");
-//        ts1.setContent(R.id.mainboard);
-//        ts2.setContent(R.id.lostboard);
-//        ts3.setContent(R.id.foundboard);
-//        ts1.setIndicator("홈");
-//        ts2.setIndicator("분실물게시판");
-//        ts3.setIndicator("습득물게시판");
-//        tabHost1.addTab(ts1);
-//        tabHost1.addTab(ts2);
-//        tabHost1.addTab(ts3);
-//
-//
-////      edittext 클릭시 검색화면으로 전환
-//        EditText editkw1 = (EditText)findViewById(R.id.editkw1) ;
-//        editkw1.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View view){
-//                Toast.makeText(HomeActivity.this, "검색창이동", Toast.LENGTH_SHORT).show();
-//                Intent in = new Intent(HomeActivity.this, SearchActivity.class);
-//                startActivity(in);
-//            }
-//        });
-//        EditText editkw2 = (EditText)findViewById(R.id.editkw2) ;
-//        editkw2.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View view){
-//                Toast.makeText(HomeActivity.this, "검색창이동", Toast.LENGTH_SHORT).show();
-//                Intent in = new Intent(HomeActivity.this, SearchActivity.class);
-//                startActivity(in);
-//            }
-//        });
-//        EditText editkw3 = (EditText)findViewById(R.id.editkw3) ;
-//        editkw3.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View view){
-//                Toast.makeText(HomeActivity.this, "검색창이동", Toast.LENGTH_SHORT).show();
-//                Intent in = new Intent(HomeActivity.this, SearchActivity.class);
-//                startActivity(in);
-//            }
-//        });
-
-
-//      분실물 게시판 데이터베이스 연동
-//        recyclerView1 = findViewById(R.id.boardrecyclerView);
-//        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(getApplicationContext(),new LinearLayoutManager(this).getOrientation());
-//        recyclerView1.addItemDecoration(dividerItemDecoration);
-//        recyclerView1.setHasFixedSize(true);
-//        layoutManager1 = new LinearLayoutManager(this);
-//        recyclerView1.setLayoutManager(layoutManager1);
-//        arrayListlost = new ArrayList<>(); //articleList 객체를 어댑터 쪽으로 담을 arrayList
-//        database = FirebaseDatabase.getInstance(); //파이어베이스 연동
-//
-//
-//        databaseReference1 = database.getReference("lost_article"); // DB 테이블 연결
-//        databaseReference1.addListenerForSingleValueEvent(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot snapshot) {
-//                //파이어베이스 데이터를 받아오는 곳
-//                arrayListlost.clear(); //기존 배열 초기화
-//                for (DataSnapshot snapshot1 : snapshot.getChildren()) { //반복문으로 데이터 리스트 추출
-//                    articlelostList articlelostList = snapshot1.getValue(articlelostList.class); //arrayList 객체에 데이터 담기
-//                    arrayListlost.add(articlelostList);
-//                }
-//                adapter1.notifyDataSetChanged(); //리스트 저장 및 새로고침
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
-//                Log.e("LoginActivity", String.valueOf(error.toException()));
-//            }
-//        });
-
-//      습득물 게시판 데이터베이스 연동
-//        recyclerView2 = findViewById(R.id.boardrecyclerView);
-//        recyclerView2.addItemDecoration(dividerItemDecoration);
-//        recyclerView2.setHasFixedSize(true);
-//        layoutManager2 = new LinearLayoutManager(this);
-//        recyclerView2.setLayoutManager(layoutManager2);
-//        arrayListfound = new ArrayList<>();
-//
-//        databaseReference2 = database.getReference("found_article");
-//        databaseReference2.addListenerForSingleValueEvent(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot snapshot) {
-//                arrayListfound.clear();
-//                for (DataSnapshot snapshot1 : snapshot.getChildren()) { //반복문으로 데이터 리스트 추출
-//                    articlefoundList articlefoundList = snapshot1.getValue(articlefoundList.class); //arrayList 객체에 데이터 담기
-//                    arrayListfound.add(articlefoundList);
-//                }
-//                adapter2.notifyDataSetChanged(); //리스트 저장 및 새로고침
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
-//                Log.e("LoginActivity", String.valueOf(error.toException()));
-//            }
-//        });
-//        adapter1 = new lostAdaptor(arrayListlost, this);
-//        adapter2 = new foundAdaptor(arrayListfound, this);
-//        recyclerView1.setAdapter(adapter1);
-//        recyclerView2.setAdapter(adapter2);
-
-
-
         BTNfound = (Button) findViewById(R.id.BTNfound);
         BTNlost = (Button) findViewById(R.id.BTNlost);
 
         BTNfound.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                callFragment(1);
-
+                boardDatabase(1);
             }
         });
         BTNlost.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                callFragment(2);
-
+                boardDatabase(2);
             }
         });
-
-
     }
     private BottomNavigationView.OnNavigationItemSelectedListener listener = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             l = (LinearLayout) findViewById(R.id.board);
+            l.setVisibility(View.GONE);
             switch (item.getItemId()) {
                     case R.id.menuwriting:
-                        l.setVisibility(View.GONE);
                         getSupportFragmentManager().beginTransaction().replace(R.id.framelayout1, fragmentWrite).commit();
                         return true;
                     case R.id.menuhome:
-                        l.setVisibility(View.GONE);
                         getSupportFragmentManager().beginTransaction().replace(R.id.framelayout1, fragmentHome).commit();
                         return true;
                     case R.id.menulistofarticles:
-                        l.setVisibility(View.GONE);
                         getSupportFragmentManager().beginTransaction().replace(R.id.framelayout1, fragmentMyarticle).commit();
                         return true;
             }
@@ -214,56 +97,22 @@ public class HomeActivity extends AppCompatActivity {
         }
     };
 
-    private void callFragment(int n) {
+    private void boardDatabase(int n) {
         l = (LinearLayout) findViewById(R.id.board);
+        l.setVisibility(View.VISIBLE);
+        recyclerView = findViewById(R.id.boardrecyclerV);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(getApplicationContext(),new LinearLayoutManager(this).getOrientation());
+        recyclerView.addItemDecoration(dividerItemDecoration);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        arrayListlost = new ArrayList<>(); //articleList 객체를 어댑터 쪽으로 담을 arrayList
+        arrayListfound = new ArrayList<>();
+        database = FirebaseDatabase.getInstance(); //파이어베이스 연동
         switch (n) {
             case 1:
-//                getSupportFragmentManager().beginTransaction().replace(R.id.framelayout1, fragmentboard).commit();
-                l.setVisibility(View.VISIBLE);
-                recyclerView1 = findViewById(R.id.boardrecyclerView);
-                DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(getApplicationContext(),new LinearLayoutManager(this).getOrientation());
-                recyclerView1.addItemDecoration(dividerItemDecoration);
-                recyclerView1.setHasFixedSize(true);
-                layoutManager1 = new LinearLayoutManager(this);
-                recyclerView1.setLayoutManager(layoutManager1);
-                arrayListlost = new ArrayList<>(); //articleList 객체를 어댑터 쪽으로 담을 arrayList
-                database = FirebaseDatabase.getInstance(); //파이어베이스 연동
-
-
-                databaseReference1 = database.getReference("lost_article"); // DB 테이블 연결
-                databaseReference1.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        //파이어베이스 데이터를 받아오는 곳
-                        arrayListlost.clear(); //기존 배열 초기화
-                        for (DataSnapshot snapshot1 : snapshot.getChildren()) { //반복문으로 데이터 리스트 추출
-                            articlelostList articlelostList = snapshot1.getValue(articlelostList.class); //arrayList 객체에 데이터 담기
-                            arrayListlost.add(articlelostList);
-                        }
-                        adapter1.notifyDataSetChanged(); //리스트 저장 및 새로고침
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-                        Log.e("LoginActivity", String.valueOf(error.toException()));
-                    }
-                });
-                adapter1 = new lostAdaptor(arrayListlost, this);
-                recyclerView1.setAdapter(adapter1);
-                break;
-            case 2:
-//                getSupportFragmentManager().beginTransaction().replace(R.id.framelayout1, fragmentboard).commit();
-                l.setVisibility(View.VISIBLE);
-                recyclerView2 = findViewById(R.id.boardrecyclerView);
-                DividerItemDecoration dividerItemDecoration1 = new DividerItemDecoration(getApplicationContext(),new LinearLayoutManager(this).getOrientation());
-                recyclerView2.addItemDecoration(dividerItemDecoration1);
-                recyclerView2.setHasFixedSize(true);
-                layoutManager2 = new LinearLayoutManager(this);
-                recyclerView2.setLayoutManager(layoutManager2);
-                arrayListfound = new ArrayList<>();
-
-                databaseReference2 = database.getReference("found_article");
-                databaseReference2.addListenerForSingleValueEvent(new ValueEventListener() {
+                databaseReference = database.getReference("found_article");
+                databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
                         arrayListfound.clear();
@@ -271,7 +120,7 @@ public class HomeActivity extends AppCompatActivity {
                             articlefoundList articlefoundList = snapshot1.getValue(articlefoundList.class); //arrayList 객체에 데이터 담기
                             arrayListfound.add(articlefoundList);
                         }
-                        adapter2.notifyDataSetChanged(); //리스트 저장 및 새로고침
+                        adapter.notifyDataSetChanged(); //리스트 저장 및 새로고침
                     }
 
                     @Override
@@ -279,8 +128,29 @@ public class HomeActivity extends AppCompatActivity {
                         Log.e("LoginActivity", String.valueOf(error.toException()));
                     }
                 });
-                adapter2 = new foundAdaptor(arrayListfound, this);
-                recyclerView2.setAdapter(adapter2);
+                adapter = new foundAdaptor(arrayListfound, this);
+                recyclerView.setAdapter(adapter);
+                break;
+            case 2:
+                databaseReference = database.getReference("lost_article"); // DB 테이블 연결
+                databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        arrayListlost.clear();
+                        for (DataSnapshot snapshot1 : snapshot.getChildren()) { //반복문으로 데이터 리스트 추출
+                            articlelostList articlelostList = snapshot1.getValue(articlelostList.class); //arrayList 객체에 데이터 담기
+                            arrayListlost.add(articlelostList);
+                        }
+                        adapter.notifyDataSetChanged(); //리스트 저장 및 새로고침
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Log.e("LoginActivity", String.valueOf(error.toException()));
+                    }
+                });
+                adapter = new lostAdaptor(arrayListlost, this);
+                recyclerView.setAdapter(adapter);
                 break;
         }
     }
