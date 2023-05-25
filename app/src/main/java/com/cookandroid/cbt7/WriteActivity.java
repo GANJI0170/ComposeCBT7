f45ecd07d70f866dc70e48282a2b350cf598ff3d
son <osj5866@gmail.com>
2023-05-25 22:50:40 +0900

    글작성 일부분

--- app/src/main/java/com/cookandroid/cbt7/WriteActivity.java
+++ app/src/main/java/com/cookandroid/cbt7/WriteActivity.java
@@ -1,15 +1,29 @@
 package com.cookandroid.cbt7;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WriteActivity extends AppCompatActivity {
+
+    private Button btn;
+    private EditText titleedit, keywordedit, dateedit, placeedit, thingedit, featureedit, detailedit;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activvity_write);
+
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
 }
