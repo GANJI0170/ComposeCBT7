f421510d29d216ae2fc753e994ba09d8f7416743
간지 <joosukho123@gmail.com>
2023-06-22 10:19:06 +0900

    알림 미완성 최종

--- app/src/main/java/com/cookandroid/cbt7/AlarmActivity.java
+++ app/src/main/java/com/cookandroid/cbt7/AlarmActivity.java
@@ -38,9 +38,9 @@ import java.util.List;
 import java.util.Map;
 
 public class AlarmActivity extends AppCompatActivity {
-    private RecyclerView recyclerView;
-    private NotificationAdapter adapter;
-    private List<NotificationItem> notificationList;
+    //    private RecyclerView recyclerView;
+//    private NotificationAdapter adapter;
+//    private List<NotificationItem> notificationList;
     private ImageButton setButton;
 
     @Override
@@ -49,14 +49,11 @@ public class AlarmActivity extends AppCompatActivity {
         setContentView(R.layout.activity_alarm);
 
         TextView test = findViewById(R.id.test);
-        test.setText("알림 페이지입니다.");
 
-        recyclerView = findViewById(R.id.notification_recycler_view);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        notificationList = new ArrayList<>();
-        adapter = new NotificationAdapter(notificationList);
-        recyclerView.setAdapter(adapter);
-        setButton = (ImageButton)findViewById(R.id.setButton);
+//        notificationList = new ArrayList<>();
+//       adapter = new NotificationAdapter(notificationList);
+//        recyclerView.setAdapter(adapter);
+        setButton = (ImageButton) findViewById(R.id.setButton);
         setButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -65,121 +62,122 @@ public class AlarmActivity extends AppCompatActivity {
             }
         });
     }
-
-    // 키워드가 등록된 사용자들에게 알림을 보내는 메서드
-    public void sendNotificationToUsers(List<String> users, String keyword) {
-        for (String userToken : users) {
-            // 알림을 보내는 메서드 호출
-            sendNotification(userToken, keyword);
-        }
-    }
-
-    // 알림을 보내는 메서드
-
-    public void sendNotification(String userToken, String keyword) {
-        // FCM 서비스 초기화
-        FirebaseMessaging fm = FirebaseMessaging.getInstance();
-
-        // 알림 내용 설정
-        String title = "새로운 습득물 등록";
-        String message = "키워드 " + keyword + "를 포함한 습득물이 등록되었습니다.";
-
-        // 알림 데이터 생성
-        Map<String, String> data = new HashMap<>();
-        data.put("title", title);
-        data.put("message", message);
-
-        // 알림 메시지 설정
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
-                .setContentTitle(title)
-                .setContentText(message)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-
-        // 알림 채널 설정 (안드로이드 8.0 이상에서 필요)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            String channelId = "channel_id";
-            String channelName = "Channel Name";
-            NotificationChannel channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT);
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-            builder.setChannelId(channelId);
-        }
-
-        // 알림 전송
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
-            return;
-        }
-        notificationManager.notify(0, builder.build());
-
-        // 알림 목록에 추가
-        notificationList.add(new NotificationItem(title, message));
-        adapter.notifyDataSetChanged();
-    }
-
-    // 알림 아이템을 표시할 ViewHolder 클래스
-    private static class NotificationViewHolder extends RecyclerView.ViewHolder {
-        private TextView titleTextView;
-        private TextView messageTextView;
-
-        public NotificationViewHolder(@NonNull View itemView) {
-            super(itemView);
-            titleTextView = itemView.findViewById(R.id.notification_title_textview);
-            messageTextView = itemView.findViewById(R.id.notification_message_textview);
-        }
-
-        public void bind(NotificationItem item) {
-            titleTextView.setText(item.getTitle());
-            messageTextView.setText(item.getMessage());
-        }
-    }
-
-    // 알림 목록을 관리할 Adapter 클래스
-    private class NotificationAdapter extends RecyclerView.Adapter<NotificationViewHolder> {
-        private List<NotificationItem> notificationList;
-
-        public NotificationAdapter(List<NotificationItem> notificationList) {
-            this.notificationList = notificationList;
-        }
-
-        @NonNull
-        @Override
-        public NotificationViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_message, parent, false);
-            return new NotificationViewHolder(view);
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull NotificationViewHolder holder, int position) {
-            NotificationItem item = notificationList.get(position);
-            holder.bind(item);
-        }
-
-        @Override
-        public int getItemCount() {
-            return notificationList.size();
-        }
-    }
-
-    // 알림 아이템 클래스
-    private static class NotificationItem {
-        private String title;
-        private String message;
-
-        public NotificationItem(String title, String message) {
-            this.title = title;
-            this.message = message;
-        }
-
-        public String getTitle() {
-            return title;
-        }
-
-        public String getMessage() {
-            return message;
-        }
-    }
 }
+
+//    // 키워드가 등록된 사용자들에게 알림을 보내는 메서드
+//    public void sendNotificationToUsers(List<String> users, String keyword) {
+//        for (String userToken : users) {
+//            // 알림을 보내는 메서드 호출
+//            sendNotification(userToken, keyword);
+//        }
+//    }
+//
+//    // 알림을 보내는 메서드
+//
+//    public void sendNotification(String userToken, String keyword) {
+//        // FCM 서비스 초기화
+//        FirebaseMessaging fm = FirebaseMessaging.getInstance();
+//
+//        // 알림 내용 설정
+//        String title = "새로운 습득물 등록";
+//        String message = "키워드 " + keyword + "를 포함한 습득물이 등록되었습니다.";
+//
+//        // 알림 데이터 생성
+//        Map<String, String> data = new HashMap<>();
+//        data.put("title", title);
+//        data.put("message", message);
+//
+//        // 알림 메시지 설정
+//        NotificationCompat.Builder builder = new NotificationCompat.Builder(this)
+//                .setContentTitle(title)
+//                .setContentText(message)
+//                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+//
+//        // 알림 채널 설정 (안드로이드 8.0 이상에서 필요)
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            String channelId = "channel_id";
+//            String channelName = "Channel Name";
+//            NotificationChannel channel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT);
+//            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+//            notificationManager.createNotificationChannel(channel);
+//            builder.setChannelId(channelId);
+//        }
+//
+//        // 알림 전송
+//        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+//        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+//            return;
+//        }
+//        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+//            return;
+//        }
+//        notificationManager.notify(0, builder.build());
+//
+//        // 알림 목록에 추가
+//        notificationList.add(new NotificationItem(title, message));
+//        adapter.notifyDataSetChanged();
+//    }
+//
+//    // 알림 아이템을 표시할 ViewHolder 클래스
+//    private static class NotificationViewHolder extends RecyclerView.ViewHolder {
+//        private TextView titleTextView;
+//        private TextView messageTextView;
+//
+//        public NotificationViewHolder(@NonNull View itemView) {
+//            super(itemView);
+//            titleTextView = itemView.findViewById(R.id.notification_title_textview);
+//            messageTextView = itemView.findViewById(R.id.notification_message_textview);
+//        }
+//
+//        public void bind(NotificationItem item) {
+//            titleTextView.setText(item.getTitle());
+//            messageTextView.setText(item.getMessage());
+//        }
+//    }
+//
+//    // 알림 목록을 관리할 Adapter 클래스
+//    private class NotificationAdapter extends RecyclerView.Adapter<NotificationViewHolder> {
+//        private List<NotificationItem> notificationList;
+//
+//        public NotificationAdapter(List<NotificationItem> notificationList) {
+//            this.notificationList = notificationList;
+//        }
+//
+//        @NonNull
+//        @Override
+//        public NotificationViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_message, parent, false);
+//            return new NotificationViewHolder(view);
+//        }
+//
+//        @Override
+//        public void onBindViewHolder(@NonNull NotificationViewHolder holder, int position) {
+//            NotificationItem item = notificationList.get(position);
+//            holder.bind(item);
+//        }
+//
+//        @Override
+//        public int getItemCount() {
+//            return notificationList.size();
+//        }
+//    }
+//
+//    // 알림 아이템 클래스
+//    private static class NotificationItem {
+//        private String title;
+//        private String message;
+//
+//        public NotificationItem(String title, String message) {
+//            this.title = title;
+//            this.message = message;
+//        }
+//
+//        public String getTitle() {
+//            return title;
+//        }
+//
+//        public String getMessage() {
+//            return message;
+//        }
+//    }
+//}
--- app/src/main/java/com/cookandroid/cbt7/KeywordActivity.java
+++ app/src/main/java/com/cookandroid/cbt7/KeywordActivity.java
@@ -6,15 +6,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
 
 public class KeywordActivity extends AppCompatActivity {
     private TextView test;
     private EditText keywordText;
     private Button button2;
+    private ChipGroup chipGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,23 +28,49 @@ public class KeywordActivity extends AppCompatActivity {
         test = findViewById(R.id.test);
         keywordText = findViewById(R.id.keywordText);
         button2 = findViewById(R.id.button2);
+        chipGroup = findViewById(R.id.chipGroup);
 
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String enteredText = keywordText.getText().toString();
 
-                // Create a new Chip
-                Chip chip = new Chip(KeywordActivity.this);
-                chip.setText(enteredText);
-                chip.setCloseIconVisible(true);
-                chip.setClickable(true);
+                if (!enteredText.isEmpty()) {
+                    // Create a new Chip
+                    Chip chip = new Chip(KeywordActivity.this);
+                    chip.setText(enteredText);
+                    chip.setCloseIconVisible(true);
+                    chip.setClickable(true);
+                    chip.setOnCloseIconClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Chip chip = (Chip) v;
+                            chipGroup.removeView(chip);
+                            // Release any resources held by the chip, if applicable
+                        }
+                    });
 
-                // Clear the EditText
-                keywordText.setText("");
+                    // Add the Chip to the ChipGroup
+                    chipGroup.addView(chip);
+
+                    // Clear the EditText
+                    keywordText.setText("");
+                }
             }
         });
 
+        // chip instances click listener
+        int chipCount = chipGroup.getChildCount();
+        for (int i = 0; i < chipCount; i++) {
+            View chipView = chipGroup.getChildAt(i);
+            if (chipView instanceof Chip) {
+                final Chip chip = (Chip) chipView;
+                chip.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                    }
+                });
+            }
+        }
     }
 }
-
